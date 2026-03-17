8234a31df091f90401cab165cf4989d4f64734cf
manveer1357 <mnvr269@gmail.com>
2026-03-17 21:37:37 +0530

    Added Rate Limiting

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -6,15 +6,19 @@ import {
   logout,
   updateProfile,
 } from "../controllers/auth.controller.js";
-
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/update-profile", protectRoute, updateProfile);
 
-router.get("/checkAuth", protectRoute, (req, res) => res.status(200).json(req.user))
+router.get("/checkAuth", protectRoute, (req, res) =>
+  res.status(200).json(req.user),
+);
 
 export default router;
